3e286f81cde623bfb480c1c95cc861bfba1cc311
André Dietrich <dietrich@ivs.cs.uni-magdeburg.de>
2017-07-11 19:24:52 +0200

    navigation buttons added

--- src/Main.elm
+++ src/Main.elm
@@ -39,11 +39,11 @@ script =
 
 ## Subtitle 1
 
-Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur,
-weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen
-Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu
-nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung,
-außer um Vorteile daraus zu ziehen? ...
+Auch *gibt es niemanden,* der den Schmerz an sich liebt, sucht oder wünscht,
+nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in
+denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales
+Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher
+Betätigung, außer um Vorteile daraus zu ziehen? ...
 
 ## Subtitle 2
 
@@ -134,10 +134,14 @@ view model =
                     , ( "float", "left" )
                     ]
                 ]
-                (model.lia
+                ((model.lia
                     |> List.indexedMap (,)
                     |> List.map
                         (\( i, l ) -> div [ onClick (Load i) ] [ a [] [ text l.title ] ])
+                 )
+                    ++ [ button [ onClick (Load (model.slide - 1)) ] [ text "<<" ]
+                       , button [ onClick (Load (model.slide + 1)) ] [ text ">>" ]
+                       ]
                 )
             , div
                 [ style
