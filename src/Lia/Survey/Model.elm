d4de9bfde592a3e8c0caf3cc6568c225d2c62926
André Dietrich <dietrich@ivs.cs.uni-magdeburg.de>
2017-09-15 20:38:54 +0200

    added json2survey encoder

--- src/Lia/Survey/Model.elm
+++ src/Lia/Survey/Model.elm
@@ -5,10 +5,12 @@ module Lia.Survey.Model
         , get_submission_state
         , get_text_state
         , get_vector_state
+        , model2json
         )
 
 import Array
 import Dict
+import Json.Encode exposing (Value, array, bool, object, string)
 import Lia.Survey.Types exposing (..)
 
 
@@ -73,3 +75,42 @@ get_matrix_state model idx row var =
 
         _ ->
             False
+
+
+model2json : SurveyVector -> Value
+model2json vector =
+    vector
+        |> Array.map element2json
+        |> array
+
+
+element2json : SurveyElement -> Value
+element2json ( b, state ) =
+    object
+        [ ( "submitted", bool b )
+        , ( "state", state2json state )
+        ]
+
+
+state2json : SurveyState -> Value
+state2json state =
+    let
+        dict2json dict =
+            dict |> Dict.toList |> List.map (\( s, b ) -> ( s, bool b )) |> object
+    in
+    object <|
+        case state of
+            TextState str ->
+                [ ( "Text", string str ) ]
+
+            SingleChoiceState vector ->
+                [ ( "SingleChoice", dict2json vector ) ]
+
+            MultiChoiceState vector ->
+                [ ( "MultiChoice", dict2json vector ) ]
+
+            SingleChoiceBlockState matrix ->
+                [ ( "SingleChoiceBlock", matrix |> Array.map dict2json |> array ) ]
+
+            MultiChoiceBlockState matrix ->
+                [ ( "MultiChoiceBlock", matrix |> Array.map dict2json |> array ) ]
--- src/Lia/Survey/Types.elm
+++ src/Lia/Survey/Types.elm
@@ -2,7 +2,6 @@ module Lia.Survey.Types exposing (..)
 
 import Array exposing (Array)
 import Dict exposing (Dict)
-import Json.Encode exposing (..)
 import Lia.Inline.Types exposing (ID, Line)
 
 
@@ -16,29 +15,6 @@ type alias SurveyVector =
 
 type alias SurveyElement =
     ( Bool, SurveyState )
-
-
-state2json state =
-    let
-        dict2json dict =
-            dict |> Dict.toList |> List.map (\( s, b ) -> ( s, bool b )) |> object
-    in
-    object <|
-        case state of
-            TextState str ->
-                [ ( "Text", string str ) ]
-
-            SingleChoiceState vector ->
-                [ ( "SingleChoice", dict2json vector ) ]
-
-            MultiChoiceState vector ->
-                [ ( "MultiChoice", dict2json vector ) ]
-
-            SingleChoiceBlockState matrix ->
-                [ ( "SingleChoiceBlock", matrix |> Array.map dict2json |> array ) ]
-
-            MultiChoiceBlockState matrix ->
-                [ ( "MultiChoiceBlock", matrix |> Array.map dict2json |> array ) ]
 
 
 type SurveyState
