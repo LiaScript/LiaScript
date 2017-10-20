882a089c2ea6322f60bc7ca7b0f94a4cbba2ea5b
André Dietrich <dietrich@ivs.cs.uni-magdeburg.de>
2017-10-20 09:51:56 +0200

    load fig gif

--- examples/Online.elm
+++ examples/Online.elm
@@ -125,7 +125,7 @@ view model =
         Loading ->
             Html.div [ style ]
                 [ Html.h2 [] [ Html.text "Loading ..." ]
-                , Html.img [ Attr.src "load.gif", Attr.width 350 ] []
+                , Html.img [ Attr.src "/fig/load.gif", Attr.width 350 ] []
 
                 --, Html.a [ Attr.href model.url, Attr.align "center" ] [ Html.text model.url ]
                 ]
