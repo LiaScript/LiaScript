0a057347b06b11a20763ca6442905ab41d02e985
Dr. André Dietrich <dietrich@ivs.cs.uni-magdeburg.de>
2018-02-06 15:40:38 +0100

    removed obsolete comments

--- src/Lia/Effect/Update.elm
+++ src/Lia/Effect/Update.elm
@@ -12,53 +12,26 @@ type Msg
     | Next
     | Previous
     | Speak
-    | Silence
     | TTS (Result String Never)
 
 
 update : Msg -> Bool -> Model -> ( Model, Cmd Msg )
 update msg sound model =
-    --    let
-    --        stop_talking model =
-    --            case model.status of
-    --                Speaking ->
-    --                    let
-    --                        c =
-    --                            Tts.Responsive.cancel ()
-    --                    in
-    --                    ( { model | status = Silent }, Cmd.none, True )
-    --
-    --                _ ->
-    --                    ( model, Cmd.none, True )
-    --    in
     case msg of
         Init ->
             update Speak sound model
 
         Next ->
             if has_next model then
-                --    stop_talking model
                 update Speak sound { model | visible = model.visible + 1 }
             else
-                --update (Speak silent)
                 ( model, Cmd.none )
 
-        --        Repeat silent ->
-        --            update (Speak silent) model
         Previous ->
             if has_previous model then
                 update Speak sound { model | visible = model.visible - 1 }
-                --stop_talking model
             else
-                --update (Speak silent)
                 ( model, Cmd.none )
-
-        Silence ->
-            let
-                c =
-                    Tts.Responsive.cancel ()
-            in
-            ( model, Cmd.none )
 
         Speak ->
             let
@@ -72,28 +45,11 @@ update msg sound model =
                 _ ->
                     ( model, Cmd.none )
 
-        --    else
-        --        ( model, Cmd.none )
-        --        Speak ->
-        --                case Dict.get model.visible model.comments of
-        --                  Just str ->
-        --                      ( model, Tts.Responsive.speak TTS model.narrator str )
-        --
-        --                _ ->
-        --                    ( model, Cmd.none, False )
         _ ->
             ( model, Cmd.none )
 
 
 
---        Speak silent ->
---            case ( get_comment model, silent ) of
---                ( Just str, False ) ->
---                    ( { model | status = Speaking }, Tts.Responsive.speak TTS model.narrator str, False )
---
---                _ ->
---                    ( model, Cmd.none, False )
---
 --        TTS (Result.Ok _) ->
 --            ( { model | status = Silent }, Cmd.none, False )
 --
@@ -124,14 +80,3 @@ next =
 previous : Msg
 previous =
     Previous
-
-
-silence : Msg
-silence =
-    Silence
-
-
-
---silence : a -> Bool
---silence b =
---    Tts.Responsive.cancel ()
