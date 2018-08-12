c0f2b146667e1830a5f59651faa64a60e07fd865
André Dietrich <andre.dietrich@ovgu.de>
2018-08-12 17:37:31 +0200

    added full highlight 2 ace translation

--- src/Lia/Code/Highlight2Ace.elm
+++ src/Lia/Code/Highlight2Ace.elm
@@ -23,40 +23,139 @@ check t lang =
 
 translations : List ( String, List String )
 translations =
-    [ ( "actionscript", [ "actionscript", "as" ] )
-    , ( "apache_conf", [ "apache", "apacheconf" ] )
-    , ( "asciidoc", [ "asciidoc", "adoc" ] )
-    , ( "assembly_x86", [ "armasm", "arm", "avrasm" ] )
-    , ( "batchfile", [ "bash", "sh", "zsh" ] )
-    , ( "c_cpp", [ "cpp", "c", "cc", "h", "c++", "h++", "hpp" ] )
-    , ( "clojure", [ "clojure", "clj" ] )
-    , ( "coffee", [ "coffeescript", "coffee", "cson", "iced" ] )
-    , ( "csharp", [ "cs", "csharp" ] )
-    , ( "diff", [ "diff", "patch" ] )
-    , ( "dockerfile", [ "dockerfile", "docker" ] )
-    , ( "erlang", [ "erl", "erlang" ] )
-    , ( "gcode", [ "gcode", "nc" ] )
-    , ( "golang", [ "go", "golang" ] )
-    , ( "handlebars", [ "handlebars", "hbs", "html.hbs", "html.handlebars" ] )
-    , ( "haskell", [ "hs", "haskell" ] )
-    , ( "haxe", [ "haxe", "hx" ] )
-    , ( "html", [ "html", "xhtml", "rss", "atom", "xjb", "xsd", "xsl", "plist" ] )
-    , ( "javascript", [ "javascript", "js" ] )
-    , ( "livescript", [ "livescript", "ls" ] )
-    , ( "makefile", [ "makefile", "mk", "mak" ] )
-    , ( "markdown", [ "markdown", "md", "mkdown", "mkd" ] )
-    , ( "ocaml", [ "ocaml", "ml" ] )
-    , ( "objectivec", [ "objectivec", "mm", "objc", "obj-c" ] )
-    , ( "php", [ "php", "php3", "php4", "php5", "php6" ] )
-    , ( "perl", [ "perl", "pl", "pm" ] )
-    , ( "powershell", [ "powershell", "ps" ] )
-    , ( "python", [ "python", "py", "gyp" ] )
-    , ( "ruby", [ "ruby", "rb", "gemspec", "podspec", "thor", "irb" ] )
-    , ( "stylus", [ "stylus", "styl" ] )
-    , ( "tcl", [ "tcl", "tk" ] )
-    , ( "twig", [ "twig", "craftcms" ] )
-    , ( "typescript", [ "typescript", "ts" ] )
-    , ( "vbscript", [ "vbscript", "vbs" ] )
-    , ( "verilog", [ "v", "verilog" ] )
-    , ( "xquery", [ "xpath", "xq" ] )
+    [ ( "actionscript"
+      , [ "actionscript", "as" ]
+      )
+    , ( "apache_conf"
+      , [ "apache", "apacheconf" ]
+      )
+    , ( "asciidoc"
+      , [ "asciidoc", "adoc" ]
+      )
+    , ( "assembly_x86"
+      , [ "x86asm", "armasm", "arm", "avrasm", "asm" ]
+      )
+    , ( "batchfile"
+      , [ "bash", "sh", "zsh" ]
+      )
+    , ( "c_cpp"
+      , [ "cpp", "c", "cc", "h", "c++", "h++", "hpp" ]
+      )
+    , ( "clojure"
+      , [ "clojure", "clj" ]
+      )
+    , ( "coffee"
+      , [ "coffeescript", "coffee", "cson", "iced" ]
+      )
+    , ( "csharp"
+      , [ "cs", "csharp", "c#" ]
+      )
+    , ( "diff"
+      , [ "diff", "patch" ]
+      )
+    , ( "django"
+      , [ "django", "jinja" ]
+      )
+    , ( "dockerfile"
+      , [ "dockerfile", "docker" ]
+      )
+    , ( "elixir"
+      , [ "elixir", "ex", "exs" ]
+      )
+    , ( "erlang"
+      , [ "erl", "erlang" ]
+      )
+    , ( "fsharp"
+      , [ "fsharp", "fs", "f#" ]
+      )
+    , ( "fortran"
+      , [ "fortran", "f90", "f95" ]
+      )
+    , ( "gcode"
+      , [ "gcode", "nc" ]
+      )
+    , ( "golang"
+      , [ "go", "golang" ]
+      )
+    , ( "handlebars"
+      , [ "handlebars", "hbs", "html.hbs", "html.handlebars" ]
+      )
+    , ( "haskell"
+      , [ "hs", "haskell" ]
+      )
+    , ( "haxe"
+      , [ "haxe", "hx" ]
+      )
+    , ( "html"
+      , [ "html", "xhtml", "rss", "atom", "xjb", "xsd", "xsl", "plist" ]
+      )
+    , ( "javascript"
+      , [ "javascript", "js" ]
+      )
+    , ( "livescript"
+      , [ "livescript", "ls" ]
+      )
+    , ( "makefile"
+      , [ "makefile", "mk", "mak" ]
+      )
+    , ( "markdown"
+      , [ "markdown", "md", "mkdown", "mkd" ]
+      )
+    , ( "ocaml"
+      , [ "ocaml", "ml" ]
+      )
+    , ( "objectivec"
+      , [ "objectivec", "mm", "objc", "obj-c" ]
+      )
+    , ( "pascal"
+      , [ "delphi", "dpr", "dfm", "pas", "pascal", "freepascal", "lazarus", "lpr", "lfm" ]
+      )
+    , ( "php"
+      , [ "php", "php3", "php4", "php5", "php6" ]
+      )
+    , ( "perl"
+      , [ "perl", "pl", "pm" ]
+      )
+    , ( "powershell"
+      , [ "powershell", "ps" ]
+      )
+    , ( "puppet"
+      , [ "puppet", "pp" ]
+      )
+    , ( "python"
+      , [ "python", "py", "gyp" ]
+      )
+    , ( "ruby"
+      , [ "ruby", "rb", "gemspec", "podspec", "thor", "irb" ]
+      )
+    , ( "rust"
+      , [ "rust", "rs" ]
+      )
+    , ( "scad"
+      , [ "scad", "openscad" ]
+      )
+    , ( "sh"
+      , [ "shell", "console" ]
+      )
+    , ( "stylus"
+      , [ "stylus", "styl" ]
+      )
+    , ( "tcl"
+      , [ "tcl", "tk" ]
+      )
+    , ( "twig"
+      , [ "twig", "craftcms" ]
+      )
+    , ( "typescript"
+      , [ "typescript", "ts" ]
+      )
+    , ( "vbscript"
+      , [ "vbscript", "vbs" ]
+      )
+    , ( "verilog"
+      , [ "v", "verilog" ]
+      )
+    , ( "xquery"
+      , [ "xpath", "xq" ]
+      )
     ]
