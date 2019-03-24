c5b7f9730722dc6eb9456d40822d5f3dfaf71d1e
André Dietrich <andre.dietrich@ovgu.de>
2019-03-24 14:31:19 +0100

    put katex into shadow dom, this avoids bug with domNode.replaceChild is not a function

--- src/javascript/webcomponents/katex.js
+++ src/javascript/webcomponents/katex.js
@@ -8,7 +8,15 @@ customElements.define('katex-formula', class extends HTMLElement {
   }
 
   connectedCallback() {
+    const shadowRoot = this.attachShadow({mode: 'open'});
+
     let span = document.createElement('span');
+    let link = document.createElement('link');
+    link.href = "formula/katex.min.css";
+    link.rel = "stylesheet";
+
+    shadowRoot.appendChild(link);
+    shadowRoot.appendChild(span);
 
     let displayMode = this.getAttribute('displayMode');
 
@@ -18,7 +26,7 @@ customElements.define('katex-formula', class extends HTMLElement {
         displayMode = JSON.parse(displayMode);
     }
 
-    katex.render(this.innerHTML, this, {
+    katex.render(this.innerHTML, span, {
         throwOnError: false,
         displayMode: displayMode
     });
