26698557537c3fa8a753b107a9ae0cff901c0d2f
André Dietrich <andre.dietrich@ovgu.de>
2019-10-21 16:12:13 +0200

    added emit to fileloader to minimize log

--- webpack.common.js
+++ webpack.common.js
@@ -79,13 +79,23 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         exclude: [/elm-stuff/, /node_modules/],
         use: [
-          'file-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              emitFile: false,
+            }
+          }
         ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              emitFile: false,
+            }
+          }
         ]
       },
       {
