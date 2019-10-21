ec959a0e4ae3427b1db7ac7d5406541746938f24
André Dietrich <andre.dietrich@ovgu.de>
2019-10-21 08:49:03 +0200

    updated svgbob invert

--- src/elm/Lia/Markdown/View.elm
+++ src/elm/Lia/Markdown/View.elm
@@ -239,15 +239,18 @@ view_block config block =
         ASCII attr txt ->
             txt
                 |> SvgBob.init SvgBob.default
-                |> SvgBob.getSvg
-                    (if config.light then
-                        attributes attr
+                |> SvgBob.getSvg (attributes attr)
+                |> (\svg ->
+                        if config.light then
+                            svg
 
-                     else
-                        attributes attr
-                            |> (::) (Svg.Attributes.style "-webkit-filter: invert(100%);")
-                            |> (::) (Svg.Attributes.style "filter: invert(100%);")
-                    )
+                        else
+                            Html.div
+                                [ Attr.style "-webkit-filter" "invert(100%)"
+                                , Attr.style "filter" "invert(100%)"
+                                ]
+                                [ svg ]
+                   )
 
 
 view_table : Config -> Annotation -> MultInlines -> List String -> List MultInlines -> Html Msg
