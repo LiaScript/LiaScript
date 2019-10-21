def8e5020e4f535bd10d395dd06b3b756f12485c
André Dietrich <andre.dietrich@ovgu.de>
2019-10-21 15:54:54 +0200

    updated loader for elm

--- webpack.common.js
+++ webpack.common.js
@@ -95,7 +95,8 @@ module.exports = {
           loader: 'elm-webpack-loader?verbose=true',
           options: {
             //debug: true,
-            //optimize: true
+            optimize: true,
+            cwd: __dirname
           },
         },
       }
