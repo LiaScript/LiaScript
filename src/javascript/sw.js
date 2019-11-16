a0027306cf0d9b1a0fabc01941bc3bcbc91365c3
André Dietrich <andre.dietrich@ovgu.de>
2019-11-16 09:11:39 +0100

    added pwa dependencies

---
+++ src/javascript/sw.js
@@ -0,0 +1,23 @@
+console.log("service-worker.js")
+
+// advanced config for injectManifest approach
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
+// Detailed logging is very useful during development
+workbox.setConfig({debug: true})
+
+// Updating SW lifecycle to update the app after user triggered refresh
+workbox.core.skipWaiting()
+workbox.core.clientsClaim()
+
+
+workbox.routing.registerRoute( /\/$/, new workbox.strategies.NetworkFirst() )
+workbox.routing.registerRoute( /\/*/, new workbox.strategies.NetworkFirst() )
+workbox.routing.registerRoute( /.+\/*/, new workbox.strategies.NetworkFirst() )
+
+workbox.routing.registerRoute(
+  /https:\/\/code\.responsivevoice\.org/,
+  new workbox.strategies.StaleWhileRevalidate()
+)
+
+workbox.precaching.precacheAndRoute([]);
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  globDirectory: "./dist",
+  globPatterns: [
+    "**/*.{ico,jpg,png,html,js,svg,webmanifest}"
+  ],
+  swDest: "./src/assets/sw.js",
+  swSrc: "./src/javascript/sw.js"
+};
