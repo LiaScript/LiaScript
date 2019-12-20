2141f09e81c785dc28595c4bb967e6b67f6cc2d5
André Dietrich <andre.dietrich@ovgu.de>
2019-12-20 14:52:29 +0100

    updated dev mode with more debug logs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import "babel-polyfill"
 
-
 import { LiaScript } from './javascript/liascript/index.js'
 
+
 if (document.getElementById('lia')) {
-  if (process.env.NODE_ENV !== 'production') {
-    var app = new LiaScript(document.getElementById('lia'), false)
-  } else {
-    var app = new LiaScript(document.getElementById('lia'), false)
-  }
+  var app = new LiaScript(
+    document.getElementById('lia'),
+    process.env.NODE_ENV !== 'production'
+  )
 }
