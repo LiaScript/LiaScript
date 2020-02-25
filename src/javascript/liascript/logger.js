140ca8d36c820af674c5c341d8447715f11cd29d
André Dietrich <andre.dietrich@ovgu.de>
2020-02-25 21:01:18 +0100

    added debug outputs

--- src/javascript/liascript/logger.js
+++ src/javascript/liascript/logger.js
@@ -1,14 +1,20 @@
 var lia = {
   log: function () {
     if (window.debug__) console.log('LiaLog: ', ...arguments)
+
+    window.liaLog(JSON.stringify(arguments))
   },
 
   warn: function () {
     if (window.debug__) console.warn('LiaWarn: ', ...arguments)
+
+    window.liaLog(JSON.stringify(arguments))
   },
 
   error: function () {
     console.error('LiaError: ', ...arguments)
+
+    window.liaLog(JSON.stringify(arguments))
   }
 }
 
