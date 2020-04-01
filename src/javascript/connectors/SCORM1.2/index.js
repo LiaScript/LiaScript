a9354a7d0422362276cba2b37fd44bfdac0e746f
André Dietrich <andre.dietrich@ovgu.de>
2020-04-01 19:06:47 +0200

    added simple interaction logging

--- src/javascript/connectors/SCORM1.2/index.js
+++ src/javascript/connectors/SCORM1.2/index.js
@@ -11,6 +11,9 @@ class Connector extends Base {
     //window.top.API = null
 
     console.log("LMSInitialize", this.scorm.LMSInitialize(""))
+
+    // store state information only in normal mode
+    this.active = this.scorm.LMSGetValue("cmi.core.lesson_mode") == "normal"
   }
 
   connect(send = null) {
@@ -33,8 +36,13 @@ class Connector extends Base {
     return this.scorm.LMSGetValue("cmi.objectives._count")
   }
 
-  countActivities() {
-    return this.scorm.LMSGetValue("cmi.activities._count")
+  countInteractions() {
+    return parseInt(this.scorm.LMSGetValue("cmi.interactions._count"))
+  }
+
+  // generates a unique id to be used in interactions and objectives
+  label(obj) {
+    return [obj.type, obj.sec + 1, obj.i].join("/")
   }
 
   getObjective(id){
@@ -42,7 +50,6 @@ class Connector extends Base {
 
     try {
       val = this.scorm.LMSGetValue("cmi.objectives."+ id +".id")
-      console.warn("WWWWWWWWWWWWWWWWW--------------------------------", val, id);
 
       let [type, sec, i, ...blob] = val.split ("/");
 
@@ -65,7 +72,7 @@ class Connector extends Base {
   }
 
   setObjective(id, obj) {
-    let blob = [obj.type, obj.sec + 1, obj.i].join("/")
+    let blob = this.label(obj)
 
     if (obj.type == "survey") {
       // use only the state info to be stored in objectives.id
@@ -73,13 +80,77 @@ class Connector extends Base {
     } else if(obj.type == "quiz") {
       blob = blob + "/" + encodeURI(JSON.stringify(obj.data))
     }
-    
+
+    console.log("setObjective", this.scorm.LMSSetValue("cmi.objectives."+ id +".id", blob))
+
+    this.scorm.LMSCommit("")
+  }
+
+  logInteraction(obj) {
+    let id = this.countInteractions()
+
+    let label = this.label(obj)
+
+    this.scorm.LMSSetValue("cmi.interactions."+ id +".id", label)
+
+    switch(obj.type) {
+      case "code": {
+        this.scorm.LMSSetValue(
+          "cmi.interactions."+ id +".student_response",
+          JSON.stringify(obj.data).slice(0,254)
+        )
+        break
+      }
+      case "quiz": {
+        this.scorm.LMSSetValue(
+          "cmi.interactions."+ id +".result",
+          obj.data.solved == 0
+            ? "neutral"
+            : ( obj.data.solved == 1
+                ? "correct"
+                : "wrong"
+              )
+        )
+
+        this.scorm.LMSSetValue(
+          "cmi.interactions."+ id +".student_response",
+          JSON.stringify(obj.data).slice(0,254)
+        )
+        break
+      }
+      case "survey": {
+        //this.scorm.LMSSetValue("cmi.interactions."+ id +".type", "fill-in")
+        this.scorm.LMSSetValue(
+          "cmi.interactions."+ id +".result",
+          "neutral"
+        )
+        this.scorm.LMSSetValue(
+          "cmi.interactions."+ id +".student_response",
+          JSON.stringify(obj.data.state).slice(0,254)
+        )
+        break
+      }
+    }
+
     this.scorm.LMSCommit("")
   }
 
   store(event) {
-    if (event.topic === "code")
+    if(!this.active)
       return
+
+    if (event.topic === "code") {
+      for(let i=0; i< event.message.length; i++) {
+        this.logInteraction({
+          type: event.topic,
+          sec: event.section,
+          i: i,
+          data: event.message[i]
+        })
+      }
+
+      return
+    }
 
     let items = []
     let count = this.countObjectives()
@@ -108,6 +179,7 @@ class Connector extends Base {
 
       // insert as new or overwrite existing ones
       this.setObjective(items.length == 0 ? this.countObjectives() : items[i], obj)
+      this.logInteraction(obj)
     }
   }
 
