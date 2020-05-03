2f1ff8626cf8e487858d85a381ed02324a7c18a1
André Dietrich <andre.dietrich@ovgu.de>
2020-05-03 20:34:36 +0200

    added check for liaLog

--- src/javascript/liascript/logger.js
+++ src/javascript/liascript/logger.js
@@ -2,19 +2,22 @@ var lia = {
   log: function () {
     if (window.debug__) console.log('LiaLog: ', ...arguments)
 
-    window.liaLog(JSON.stringify(arguments))
+    if (window.liaLog)
+      window.liaLog(JSON.stringify(arguments))
   },
 
   warn: function () {
     if (window.debug__) console.warn('LiaWarn: ', ...arguments)
 
-    window.liaLog(JSON.stringify(arguments))
+    if (window.liaLog)
+      window.liaLog(JSON.stringify(arguments))
   },
 
   error: function () {
     console.error('LiaError: ', ...arguments)
 
-    window.liaLog(JSON.stringify(arguments))
+    if (window.liaLog)
+      window.liaLog(JSON.stringify(arguments))
   }
 }
 
