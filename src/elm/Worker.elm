43fb8b1e211a7defb4e1319f6adb497d529e78f3
André Dietrich <andre.dietrich@ovgu.de>
2020-05-10 18:07:49 +0200

    added service worker

--- src/elm/Worker.elm
+++ src/elm/Worker.elm
@@ -3,9 +3,11 @@ port module Worker exposing (init)
 import Array
 import Http
 import Json.Encode as JE
+import Lia.Definition.Json.Encode as Def
 import Lia.Json.Encode as Lia
 import Lia.Markdown.Quiz.Json as Quiz
 import Lia.Markdown.Survey.Json as Survey
+import Lia.Parser.Parser as Parser
 import Lia.Script
 import Lia.Update exposing (generate)
 import Model exposing (State(..))
@@ -70,6 +72,15 @@ message msg =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Handle [ "defines", readme ] ->
+            ( model
+            , readme
+                |> Parser.parse_defintion ""
+                |> Result.map (Tuple.first >> Def.encode >> JE.encode 2 >> Tuple.pair True)
+                |> Result.withDefault ( False, "" )
+                |> output
+            )
+
         Handle [ cmd, readme ] ->
             load_readme readme { model | cmd = cmd }
 
---
+++ src/entry/worker/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+import { Elm } from '../../elm/Worker.elm'
+
+module.exports = Elm.Worker;
