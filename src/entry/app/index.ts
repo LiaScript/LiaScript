363701517d6a1e1f6729edc122ea242cd92b852f
André Dietrich <andredietrich@web.de>
2021-01-05 07:00:03 +0100

    moved entry app/index to ts

--- src/entry/app/index.ts
+++ src/entry/app/index.ts
@@ -1,14 +1,17 @@
 import "@babel/polyfill"
 
-import LiaScript from '../../javascript/liascript/index.ts'
-import { Connector } from '../../javascript/connectors/Browser/index.ts'
+import LiaScript from '../../javascript/liascript/index'
+import { Connector } from '../../javascript/connectors/Browser/index'
 
 const ua = window.navigator.userAgent
 
 if (ua.indexOf('Trident/') > 0 ||
   ua.indexOf('MSIE ') > 0) {
   console.warn('unsupported browser')
-  document.getElementById('IE-message').hidden = false
+
+  const elem = document.getElementById('IE-message')
+
+  if (elem) elem.hidden = false
 } else {
   let debug = false
 
