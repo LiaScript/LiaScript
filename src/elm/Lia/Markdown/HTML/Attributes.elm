d1a56975db3f7feb0dbfef367ec927b3ed7e94b3
André Dietrich <andredietrich@web.de>
2021-01-22 15:30:10 +0100

    docs(HTML): Add comments to HTML Attribute parser

--- src/elm/Lia/Markdown/HTML/Attributes.elm
+++ src/elm/Lia/Markdown/HTML/Attributes.elm
@@ -36,12 +36,21 @@ import Html.Attributes as Attr
 import Lia.Markdown.HTML.NamedCharacterReferences as NamedCharacterReferences
 
 
+{-| Used internaly to store HTML parameter as Tuples of `(name, value)`.
+
+**Note:** The name value is automatically stored in lowercase.
+
+-}
 type alias Parameters =
     List ( String, String )
 
 
 {-| Search the attribute list for a certain key, the corresponding value will be
 returned, if present.
+
+    get "height" [ ( "width", "100%" ), ( "height", "300px" ) ]
+        == Just "300px"
+
 -}
 get : String -> Parameters -> Maybe String
 get name attr =
@@ -57,47 +66,124 @@ get name attr =
                 get name xs
 
 
+{-| Filter a list of Parameters with a list of names, so that only Parameters
+get returned that are within the filter list:
+
+    filterNames
+        [ "height" ]
+        [ ( "width", "100%" ), ( "height", "300px" ) ]
+        == [ ( "height", "300px" ) ]
+
+-}
 filterNames : List String -> Parameters -> Parameters
 filterNames names =
-    List.filter (filter_ names)
+    List.filter (isMemberOf names)
 
 
-filter_ : List String -> ( String, x ) -> Bool
-filter_ names ( name, _ ) =
+isMemberOf : List String -> ( String, x ) -> Bool
+isMemberOf names ( name, _ ) =
     List.member name names
 
 
+{-| Return True or False, if a parameter is set in the parameter list:
+
+    isSet "data" [ ( "data", "TrUe" ) ] == True
+
+    isSet "data" [ ( "data", "1" ) ] == True
+
+    isSet "data" [ ( "data", "" ) ] == True
+
+    isSet "data" [ ( "data", "Moodle" ) ] == False
+
+    isSet "data" [ ( "dat", "Moodle" ) ] == False
+
+-}
 isSet : String -> Parameters -> Bool
 isSet name =
     isSetMaybe name >> Maybe.withDefault False
 
 
+{-| Return `Maybe True` or `Maybe False`, if a parameter is set in the parameter
+list:
+
+    isSetMaybe "data" [ ( "data", "TrUe" ) ] == Just True
+
+    isSetMaybe "data" [ ( "data", "1" ) ] == Just True
+
+    isSetMaybe "data" [ ( "data", "" ) ] == Just True
+
+    isSetMaybe "data" [ ( "data", "Moodle" ) ] == Just False
+
+    isSetMaybe "data" [ ( "dat", "Moodle" ) ] == Nothing
+
+-}
 isSetMaybe : String -> Parameters -> Maybe Bool
 isSetMaybe name =
     get name
         >> Maybe.map (String.trim >> String.toLower >> isTrue)
 
 
+{-| Return True if the parameter is **Not** set in the parameter list:
+
+    isNotSet "data" [ ( "data", "TrUe" ) ] == True
+
+    isNotSet "data" [ ( "data", "1" ) ] == True
+
+    isNotSet "data" [ ( "data", "" ) ] == True
+
+    isNotSet "data" [ ( "data", "Moodle" ) ] == False
+
+    isNotSet "data" [ ( "dat", "Moodle" ) ] == True
+
+-}
 isNotSet : String -> Parameters -> Bool
 isNotSet name =
     isSetMaybe name >> Maybe.withDefault True
 
 
+{-| **@private:** Check if a parameter is set to true, which is the case if the
+passed string is somehow set to true:
+
+    isTrue "" == True
+
+    isTrue "1" == True
+
+    isTrue "true" == True
+
+    isTrue "everything else" == False
+
+-}
 isTrue : String -> Bool
 isTrue val =
     val == "" || val == "1" || val == "true"
 
 
+{-| Add the classname and to the parameter list and translate them into a list
+of attributes...
+-}
 annotation : String -> Parameters -> List (Attribute msg)
 annotation cls =
-    (::) ( "class", "lia-inline " ++ cls ) >> toAttribute
+    (::) ( "class", cls ) >> toAttribute
 
 
+{-| Translate a list of Parameters into HTML attributes.
+-}
 toAttribute : Parameters -> List (Attribute msg)
 toAttribute =
     List.map (\( key, value ) -> Attr.attribute key value)
 
 
+{-| Parameters of type:
+
+    "src", "href", "data", "data-src", "formaction", "poster"
+
+are checked if they definee a relative or absolute path. If it is a relative
+URL, then the base-URL is automatically added to the front:
+
+    base "http://base.url/" ( "src", "pic.jpg" )
+        == ( "src", "http://base.url/pic.jpg" )
+
+-}
 base : String -> ( String, String ) -> ( String, String )
 base url ( key, value ) =
     ( key
@@ -122,15 +208,36 @@ base url ( key, value ) =
     )
 
 
+{-| Used to identify relative paths. If the URL does not start with "http", the
+basis is automatically added:
+
+    toUrl "https://base.url/" "pic.jpg"
+        == "https://base.url/pic.jpg"
+
+    toUtl "https://base.url/" "http://url.de/pic.jpg"
+        == "http://url.de/pic.jpg"
+
+-}
 toURL : String -> String -> String
 toURL basis url =
-    if String.startsWith "http" url then
+    if
+        url
+            |> String.toLower
+            |> String.startsWith "http"
+    then
         url
 
     else
         basis ++ url
 
 
+{-| General HTML attribute parser, the base-URL is added in front of relative
+paths, as defined in function `base`:
+
+    Combine.parse (parse "http://base.url/" "SRC = 'img.jpg'")
+        == Ok ( "src", "http://base.url/img.jpg" )
+
+-}
 parse : String -> Parser context ( String, String )
 parse url =
     regex "[A-Za-z0-9_\\-]+"
@@ -141,6 +248,25 @@ parse url =
         |> map (base url)
 
 
+{-| **@private**: A tag attribute-value can be set with:
+
+  - single quote:
+
+        parse tagAttributeValue "= '100%'" == Ok "100%"
+
+  - double quotes:
+
+        parse tagAttributeValue "= \"100%\"" == Ok "100%"
+
+  - no quotes:
+
+        parse tagAttributeValue "= 100%" == Ok "100%"
+
+  - or not set:
+
+        parse tagAttributeValue "    " == Ok ""
+
+-}
 tagAttributeValue : Parser context String
 tagAttributeValue =
     or
@@ -218,6 +344,13 @@ numericCharacterReference =
         |> keep (map (Char.fromCode >> String.fromChar) codepoint)
 
 
+{-| **@private: ** Hexadecimal Strings are converted to Int
+
+    parse hexadecimal "FF" == Ok 255
+
+    parse hexadecimal "QQ" == Err "Invalid hexadecimal string"
+
+-}
 hexadecimal : Parser context Int
 hexadecimal =
     regex "[0-9a-fA-F]+"
