ef397a5de0661d6ad0b21fa41858af4cc8b7c8b6
André Dietrich <andredietrich@web.de>
2021-03-30 14:47:08 +0200

    refact(Index): Implements basic card structure

--- src/elm/Index/View.elm
+++ src/elm/Index/View.elm
@@ -1,14 +1,14 @@
 module Index.View exposing (view)
 
-import Accessibility.Key exposing (tabbable)
+import Accessibility.Role exposing (definition)
 import Array
 import Const
-import Dict
+import Dict exposing (Dict)
 import Html exposing (Attribute, Html)
 import Html.Attributes as Attr exposing (title)
-import Html.Events exposing (onClick)
 import Index.Model exposing (Course, Model, Release)
 import Index.Update exposing (Msg(..))
+import Lia.Definition.Types exposing (Definition)
 import Lia.Markdown.Code.Editor exposing (onChange)
 import Lia.Markdown.Inline.Stringify exposing (stringify)
 import Lia.Markdown.Inline.Types exposing (Inlines)
@@ -25,8 +25,8 @@ view session model =
     Html.div []
         [ Html.h1 [] [ Html.text "Lia: Open-courSes" ]
         , searchBar model.input
-        , Html.div [] <|
-            if List.isEmpty model.courses && model.initialized then
+        , if List.isEmpty model.courses && model.initialized then
+            Html.div [] <|
                 [ [ "If you cannot see any courses in this list, try out one of the following links, to get more information about this project and to visit some examples and free interactive books."
                         |> Html.text
                   ]
@@ -45,12 +45,13 @@ view session model =
                 , Html.text "Have a nice one ;-) ..."
                 ]
 
-            else if model.initialized then
-                model.courses
-                    |> List.map (card session.share)
+          else if model.initialized then
+            model.courses
+                |> List.map (card session.share)
+                |> Html.div [ Attr.class "preview-grid" ]
 
-            else
-                []
+          else
+            Html.text ""
         ]
 
 
@@ -70,6 +71,7 @@ searchBar url =
         ]
 
 
+getIcon : Dict String String -> String
 getIcon =
     Dict.get "icon"
         >> Maybe.withDefault Const.icon
@@ -79,109 +81,17 @@ card : Bool -> Course -> Html Msg
 card share course =
     case get_active course of
         Just { title, definition } ->
-            [ case ( String.trim definition.author, String.trim definition.logo ) of
-                ( str_a, "" ) ->
-                    Html.div []
-                        [ Html.img
-                            [ definition.macro
-                                |> getIcon
-                                |> Attr.src
-                            ]
-                            []
-                        , author str_a
-                        ]
-
-                ( str_a, str_l ) ->
-                    Html.div []
-                        [ Html.img
-                            [ definition.macro
-                                |> getIcon
-                                |> Attr.src
-                            ]
-                            []
-                        , Html.img [ Attr.src str_l ] []
-                        , author str_a
-                        ]
-            , [ title
-                    |> inlines
-                    |> Html.p
-                        [ Attr.style "white-space" "nowrap"
-                        , Attr.style "overflow" "hidden"
-                        , Attr.style "text-overflow" "ellipsis"
-                        ]
-              , definition.comment
-                    |> inlines
-                    |> Html.p []
-              ]
-                |> Html.div []
-            , Html.hr [] []
-            , [ viewVersions course
-              , Html.text course.last_visit
-              ]
-                |> Html.div []
-            , btn
-                { msg = Just <| Delete course.id
-                , title = "delete"
-                , tabbable = True
-                }
-                []
-                [ Html.text "Delete" ]
-            , btn
-                { msg = Just <| Reset course.id course.active
-                , title = "reset"
-                , tabbable = True
-                }
-                []
-                [ Html.text "Reset" ]
-            , if share then
-                btn
-                    { msg =
-                        Just <|
-                            Share
-                                (title |> stringify)
-                                ((definition.comment |> stringify) ++ "\n")
-                                ("https://LiaScript.github.io/course/?" ++ course.id)
-                    , title = "share"
-                    , tabbable = True
-                    }
-                    []
-                    [ Html.text "Share" ]
-
-              else
-                Html.text ""
-            , case course.active of
-                Nothing ->
-                    Html.a
-                        [ href course.id
-                        , Attr.class "lia-btn"
-                        ]
-                        [ Html.text "Open" ]
-
-                Just _ ->
-                    btn
-                        { msg = Just <| Restore course.id course.active
-                        , title = "open"
-                        , tabbable = True
-                        }
-                        []
-                        [ Html.text "Open"
-                        ]
-            ]
-                |> Html.div []
+            Html.article [ Attr.class "card" ]
+                [ viewVersions course
+                , viewMedia definition.logo
+                , viewHeader title definition.macro
+                , viewBody definition.comment
+                , viewControls share title definition.comment course
+                , viewFooter definition
+                ]
 
         _ ->
             Html.text "something went wrong"
-
-
-author : String -> Html msg
-author str =
-    (if str == "" then
-        "Author: by Annonymous"
-
-     else
-        "Author: by " ++ str
-    )
-        |> Html.text
 
 
 href : String -> Attribute msg
@@ -201,23 +111,6 @@ viewVersions course =
         |> List.sortBy Tuple.first
         |> List.indexedMap
             (\i ( key, value ) ->
-                let
-                    color =
-                        case course.active of
-                            Just active ->
-                                if active == key then
-                                    "#000"
-
-                                else
-                                    "#888"
-
-                            Nothing ->
-                                if last == i then
-                                    "#000"
-
-                                else
-                                    "#888"
-                in
                 btn
                     { msg =
                         Just <|
@@ -231,13 +124,164 @@ viewVersions course =
                     , tabbable = True
                     , title = "load version " ++ value
                     }
-                    [ Attr.style "color" color
-                    , Attr.class "lia-btn--outline"
+                    [ Attr.class <|
+                        case course.active of
+                            Just id ->
+                                if id == key then
+                                    "active"
+
+                                else
+                                    ""
+
+                            Nothing ->
+                                if last == i then
+                                    "active"
+
+                                else
+                                    ""
                     ]
                     [ "V " ++ value |> Html.text
                     ]
             )
-        |> Html.div []
+        |> Html.div [ Attr.class "card__version" ]
+
+
+viewMedia : String -> Html msg
+viewMedia url =
+    Html.div [ Attr.class "card__media" ]
+        [ Html.aside [ Attr.class "card__aside" ]
+            [ Html.figure [ Attr.class "card__figure" ]
+                [ Html.img
+                    [ Attr.class "card__image"
+                    , Attr.src <|
+                        case String.trim url of
+                            "" ->
+                                ""
+
+                            logo ->
+                                logo
+                    ]
+                    []
+                ]
+            ]
+        ]
+
+
+viewHeader : Inlines -> Dict String String -> Html Msg
+viewHeader title macro =
+    Html.header [ Attr.class "card__header" ]
+        [ title
+            |> inlines
+            |> Html.h3 [ Attr.class "card__title" ]
+        , macro
+            |> Dict.get "tags"
+            |> Maybe.map
+                (String.replace ";" " | "
+                    >> Html.text
+                    >> List.singleton
+                    >> Html.h4 [ Attr.class "card__subtitle" ]
+                )
+            |> Maybe.withDefault (Html.text "")
+        ]
+
+
+viewBody : Inlines -> Html Msg
+viewBody comment =
+    Html.div [ Attr.class "card__body" ] <|
+        case comment of
+            [] ->
+                []
+
+            _ ->
+                [ comment
+                    |> inlines
+                    |> Html.p [ Attr.class "card_copy" ]
+                ]
+
+
+viewControls : Bool -> Inlines -> Inlines -> Course -> Html Msg
+viewControls hasShareAPI title comment course =
+    Html.div []
+        [ btn
+            { msg = Just <| Delete course.id
+            , title = "delete"
+            , tabbable = True
+            }
+            []
+            [ Html.text "Delete" ]
+        , btn
+            { msg = Just <| Reset course.id course.active
+            , title = "reset"
+            , tabbable = True
+            }
+            []
+            [ Html.text "Reset" ]
+        , if hasShareAPI then
+            btn
+                { msg =
+                    Just <|
+                        Share
+                            (title |> stringify)
+                            ((comment |> stringify) ++ "\n")
+                            ("https://LiaScript.github.io/course/?" ++ course.id)
+                , title = "share"
+                , tabbable = True
+                }
+                []
+                [ Html.text "Share" ]
+
+          else
+            Html.text ""
+        , case course.active of
+            Nothing ->
+                Html.a
+                    [ href course.id
+                    , Attr.class "lia-btn"
+                    ]
+                    [ Html.text "Open" ]
+
+            Just _ ->
+                btn
+                    { msg = Just <| Restore course.id course.active
+                    , title = "open"
+                    , tabbable = True
+                    }
+                    []
+                    [ Html.text "Open"
+                    ]
+        ]
+
+
+viewFooter : Definition -> Html msg
+viewFooter definition =
+    Html.footer [ Attr.class "card__footer" ]
+        [ Html.img
+            [ Attr.class "card__logo"
+            , definition.macro
+                |> getIcon
+                |> Attr.src
+            , Attr.alt "Logo"
+            , Attr.height 50
+            ]
+            []
+        , case ( String.trim definition.author, String.trim definition.email ) of
+            ( "", "" ) ->
+                Html.text ""
+
+            ( "", email ) ->
+                contact email email
+
+            ( author, "" ) ->
+                Html.span [ Attr.class "card__contact" ] [ Html.text author ]
+
+            ( author, email ) ->
+                contact author email
+        ]
+
+
+contact : String -> String -> Html msg
+contact title mail =
+    Html.a [ Attr.class "card__contact", Attr.href <| "mailto:" ++ mail ] [ Html.text title ]
 
 
 get_active : Course -> Maybe Release
