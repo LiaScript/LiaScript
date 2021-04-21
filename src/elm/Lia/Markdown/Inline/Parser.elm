01a6377c9161fde31b7cd63bcea4e5366de283f1
André Dietrich <andredietrich@web.de>
2021-04-21 15:28:29 +0200

    fix #45: Greedy quotation " parsing for inlines

--- src/elm/Lia/Markdown/Inline/Parser.elm
+++ src/elm/Lia/Markdown/Inline/Parser.elm
@@ -487,7 +487,7 @@ stringSuperscript =
 
 stringCharacters : Parser s (Parameters -> Inline)
 stringCharacters =
-    regex "[~:_;=${}\\[\\]\\(\\)\\-+]"
+    regex "[~:_;=${}\\[\\]\\(\\)\\-+\"]"
         |> map Chars
 
 
