39bb9029c4660291c1a5178f82a6d331389bc019
André Dietrich <andredietrich@web.de>
2021-04-23 21:09:14 +0200

    improve(EmptyError): Add basic example

--- src/elm/Error/Message.elm
+++ src/elm/Error/Message.elm
@@ -104,7 +104,169 @@ For more information visit some of the following sources:
 
 emptyFile : String
 emptyFile =
-    """> The file you want me to load does not contain any content. Everything I see is only an empty string..."""
+    """> The file you want me to load does not contain any content. Everything I see is only an empty string...
+    
+If you see this in message in an editor, try to copy and paste the following code:
+
+```` md
+<!--
+author:   Your Name
+
+email:    your@mail.org
+
+version:  0.0.1
+
+language: en
+
+narrator: US English Female
+
+comment:  Try to write a short comment about
+          your course, multiline is also okay.
+-->
+
+# Course Main Title
+
+This is your **course** initialization stub.
+
+Please see the [Docs](https://liascript.github.io/course/?https://raw.githubusercontent.com/liaScript/docs/master/README.md)
+to find out what is possible in [LiaScript](https://liascript.github.io).
+
+If you want to use instant help in your Atom IDE, please type **lia** to see all available shortcuts.
+
+## Markdown
+
+You can use common [Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) syntax to create your course, such as:
+
+1. Lists
+2. ordered or
+
+   * unordered
+   * ones ...
+
+
+| Header 1   | Header 2   |
+| :--------- | :--------- |
+| Item 1     | Item 2     |
+
+
+Images:
+
+![images](https://farm2.static.flickr.com/1618/26701766821_7bea494826.jpg)
+
+
+### Extensions
+
+     --{{0}}--
+But you can also include other features such as spoken text.
+
+      --{{1}}--
+Insert any kind of audio file:
+
+       {{1}}
+?[audio](https://bigsoundbank.com/UPLOAD/mp3/1068.mp3)
+
+
+     --{{2}}--
+Even videos or change the language completely.
+
+       {{2-3}}
+!?[video](https://www.youtube.com/watch?v=bICfKRyKTwE)
+
+
+      --{{3 Russian Female}}--
+Первоначально создан в 2004 году Джоном Грубером (англ. John Gruber) и Аароном
+Шварцем. Многие идеи языка были позаимствованы из существующих соглашений по
+разметке текста в электронных письмах...
+
+
+    {{3}}
+Type "voice" to see a list of all available languages.
+
+
+### Styling
+
+<!-- class = "animated rollIn" style = "animation-delay: 2s; color: purple" -->
+The whole text-block should appear in purple color and with a wobbling effect.
+Which is a **bad** example, please use it with caution ...
+~~ only this is red ;-) ~~ <!-- class = "animated infinite bounce" style = "color: red;" -->
+
+## Charts
+
+Use ASCII-Art to draw diagrams:
+
+                                    Multiline
+    1.9 |    DOTS
+        |                 ***
+      y |               *     *
+      - | r r r r r r r*r r r r*r r r r r r r
+      a |             *         *
+      x |            *           *
+      i | B B B B B * B B B B B B * B B B B B
+      s |         *                 *
+        | *  * *                       * *  *
+     -1 +------------------------------------
+        0              x-axis               1
+
+## Quizzes
+
+### A Textquiz
+
+What did the **fish** say when he hit a **concrete wall**?
+
+    [[dam]]
+
+### Multiple Choice
+
+Just add as many points as you wish:
+
+    [[X]] Only the **X** marks the correct point.
+    [[ ]] Empty ones are wrong.
+    [[X]] ...
+
+### Single Choice
+
+Just add as many points as you wish:
+
+    [( )] ...
+    [(X)] <-- Only the **X** is allowed.
+    [( )] ...
+
+
+## Executable Code
+
+You can make your code executable and define projects:
+
+``` js     -EvalScript.js
+let who = data.first_name + " " + data.last_name;
+
+if(data.online) {
+  who + " is online"; }
+else {
+  who + " is NOT online"; }
+```
+``` json    +Data.json
+{
+  "first_name" :  "Sammy",
+  "last_name"  :  "Shark",
+  "online"     :  true
+}
+```
+<script>
+  // insert the JSON dataset into the local variable data
+  let data = @input(1);
+
+  // eval the script that uses this dataset
+  eval(`@input(0)`);
+</script>
+
+
+## More
+
+Find out what you also can do ...
+
+https://liascript.github.io/course/?https://raw.githubusercontent.com/liaScript/docs/master/README.md
+````
+"""
 
 
 parseDefinintion : String -> String -> String
