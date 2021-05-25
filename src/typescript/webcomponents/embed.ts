d77f95e2a110abb5afb026dcf4095b3517d59ac2
André Dietrich <andredietrich@web.de>
2021-05-25 19:12:58 +0200

    style: Prettify embed.ts

--- src/typescript/webcomponents/embed.ts
+++ src/typescript/webcomponents/embed.ts
@@ -1,124 +1,128 @@
-import {
-  extract,
-  setProviderList
-} from 'oembed-parser';
+import { extract, setProviderList } from "oembed-parser";
 
-const providers = require('./embed-providers.json');
+const providers = require("./embed-providers.json");
 
-setProviderList(providers)
+setProviderList(providers);
 
 customElements.define(
-  'lia-embed',
+  "lia-embed",
   class extends HTMLElement {
-
-    private url_: string | null
-    private div_: HTMLDivElement
-    private maxwidth_: number | null
-    private maxheight_: number | null
-    private paramCount : number
+    private url_: string | null;
+    private div_: HTMLDivElement;
+    private maxwidth_: number | null;
+    private maxheight_: number | null;
+    private paramCount: number;
 
     constructor() {
-      super()
+      super();
 
-      this.url_ = null
+      this.url_ = null;
 
-      this.div_ = document.createElement('div')
-      this.div_.style.width = "inherit"
-      this.div_.style.height = "inherit"
-      this.div_.style.display = "inline-block"
-      
-      this.maxheight_ = null
-      this.maxwidth_ = null
+      this.div_ = document.createElement("div");
+      this.div_.style.width = "inherit";
+      this.div_.style.height = "inherit";
+      this.div_.style.display = "inline-block";
 
-      this.paramCount = 0
+      this.maxheight_ = null;
+      this.maxwidth_ = null;
+
+      this.paramCount = 0;
     }
 
     connectedCallback() {
       let shadowRoot = this.attachShadow({
-        mode: 'closed',
-      })      
+        mode: "closed",
+      });
 
-      shadowRoot.appendChild(this.div_)
+      shadowRoot.appendChild(this.div_);
 
-      const container = this.parentElement // document.getElementsByClassName("lia-slide__content")[0]
+      const container = this.parentElement; // document.getElementsByClassName("lia-slide__content")[0]
 
-      let scale = null
+      let scale = parseFloat(this.getAttribute("scale") || "0.674");
 
-      try {
-        scale = parseFloat(this.getAttribute("scale"))
-      } catch (e) {}
+      if (container) {
+        const paddingLeft = parseInt(
+          window
+            .getComputedStyle(container)
+            .getPropertyValue("padding-left")
+            .replace("px", "")
+        );
 
-      if(container) {
-        const paddingLeft = parseInt(window.getComputedStyle(container).getPropertyValue('padding-left').replace("px", ""))
+        this.maxwidth_ =
+          this.maxwidth_ != null
+            ? this.maxwidth_
+            : container.clientWidth - paddingLeft - 30;
+        this.maxheight_ =
+          this.maxheight_ != null
+            ? this.maxheight_
+            : Math.floor(this.maxwidth_ * (scale || 0.674));
 
-        this.maxwidth_ = this.maxwidth_ != null ? this.maxwidth_ : container.clientWidth - paddingLeft - 30
-        this.maxheight_ = this.maxheight_ != null ? this.maxheight_ : Math.floor(this.maxwidth_ * (scale || 0.674))
-
-        if ( this.maxheight_ > screen.availHeight ) {
-          this.maxheight_ = Math.floor(screen.availHeight * (scale || 0.76))
+        if (this.maxheight_ > screen.availHeight) {
+          this.maxheight_ = Math.floor(screen.availHeight * (scale || 0.76));
         }
       }
 
-      this.render()
+      this.render();
     }
 
     render() {
-      if (this.paramCount > 2) {  
-
-        let div = this.div_
-        let options = { 
+      if (this.paramCount > 2) {
+        let div = this.div_;
+        let options = {
           maxwidth: this.maxwidth_,
-          maxheight: this.maxheight_
-        }
+          maxheight: this.maxheight_,
+        };
 
         extract(this.url_, options)
-        .then((json: any) => {
-          div.innerHTML = json.html
-        })
-        .catch((err: any) => {
-          div.innerHTML = `<iframe src="${this.url_}" style="width: ${options.maxwidth ? options.maxwidth+"px" : "100%"}; height: ${options.maxheight ? options.maxheight+"px" : "inherit"};" allowfullscreen loading="lazy"></iframe>`
-
-        });
+          .then((json: any) => {
+            div.innerHTML = json.html;
+          })
+          .catch((err: any) => {
+            div.innerHTML = `<iframe src="${this.url_}" style="width: ${
+              options.maxwidth ? options.maxwidth + "px" : "100%"
+            }; height: ${
+              options.maxheight ? options.maxheight + "px" : "inherit"
+            };" allowfullscreen loading="lazy"></iframe>`;
+          });
       }
     }
 
     get url() {
-      return this.url_
+      return this.url_;
     }
 
     set url(value) {
       if (this.url_ !== value) {
-        this.url_ = value
-        this.paramCount++
-        this.render()
+        this.url_ = value;
+        this.paramCount++;
+        this.render();
       }
     }
 
     get maxheight() {
-      return this.maxheight_
+      return this.maxheight_;
     }
 
     set maxheight(value) {
       if (this.maxheight_ !== value) {
-        this.paramCount++
+        this.paramCount++;
         if (value != 0) {
-          this.maxheight_ = value
+          this.maxheight_ = value;
         }
       }
     }
 
     get maxwidth() {
-      return this.maxwidth_
+      return this.maxwidth_;
     }
 
     set maxwidth(value) {
-      if (this.maxwidth_ !== value) {     
-        this.paramCount++
+      if (this.maxwidth_ !== value) {
+        this.paramCount++;
         if (value != 0) {
-          this.maxwidth_ = value
+          this.maxwidth_ = value;
         }
       }
     }
-  },
-)
-
+  }
+);
