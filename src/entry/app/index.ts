3fa19ca18eee28eb14757a84d1e644ad2c5dd04c
André Dietrich <andredietrich@web.de>
2021-06-29 14:36:04 +0200

    Add code-splitting for connectors

--- src/entry/app/index.ts
+++ src/entry/app/index.ts
@@ -1,20 +1,23 @@
 import LiaScript from '../../typescript/liascript/index'
-import { Connector } from '../../typescript/connectors/Browser/index'
 
-const ua = window.navigator.userAgent
+import('../../typescript/connectors/Browser/index').then(function (Browser) {
+  const ua = window.navigator.userAgent
 
-if (ua.indexOf('Trident/') > 0 || ua.indexOf('MSIE ') > 0) {
-  console.warn('unsupported browser')
+  if (ua.indexOf('Trident/') > 0 || ua.indexOf('MSIE ') > 0) {
+    console.warn('unsupported browser')
 
-  const elem = document.getElementById('IE-message')
+    const elem = document.getElementById('IE-message')
 
-  if (elem) elem.hidden = false
-} else {
-  let debug = false
+    if (elem) elem.hidden = false
+  } else {
+    let debug = false
 
-  if (process.env.NODE_ENV === 'development') {
-    debug = true
+    if (process.env.NODE_ENV === 'development') {
+      debug = true
+    }
+
+    const app = new LiaScript(document.body, new Browser.Connector(), debug)
   }
+})
 
-  const app = new LiaScript(document.body, new Connector(), debug)
-}
+
