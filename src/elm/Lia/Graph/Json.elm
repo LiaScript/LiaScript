bf7e184275a3306c14962c611f3063d0d7d80b3e
André Dietrich <andredietrich@web.de>
2021-08-02 09:32:59 +0200

    feat(Graph): Add json encoding

---
+++ src/elm/Lia/Graph/Json.elm
@@ -0,0 +1,74 @@
+module Lia.Graph.Json exposing (encode)
+
+import Dict
+import Json.Encode as JE
+import Lia.Graph.Model
+    exposing
+        ( Edge
+        , Graph
+        , Node(..)
+        )
+
+
+encode : Graph -> JE.Value
+encode graph =
+    JE.object
+        [ ( "node"
+          , graph.node
+                |> Dict.map (\_ ( node, _ ) -> node)
+                |> JE.dict identity fromNode
+          )
+        , ( "edge", JE.list fromEdge graph.edge )
+        ]
+
+
+fromEdge : Edge -> JE.Value
+fromEdge edge =
+    JE.list JE.string [ edge.from, edge.to ]
+
+
+fromNode : Node -> JE.Value
+fromNode node =
+    case node of
+        Section sec ->
+            [ fromName sec
+            , ( "id", JE.int sec.id )
+            , ( "indentation", JE.int sec.indentation )
+            , ( "weight", JE.int sec.weight )
+            ]
+                |> JE.object
+                |> fromType "sec"
+
+        Link link ->
+            [ fromName link
+            , fromUrl link
+            ]
+                |> JE.object
+                |> fromType "url"
+
+        Hashtag tag ->
+            tag
+                |> JE.string
+                |> fromType "tag"
+
+        Course course ->
+            [ fromName course
+            , fromUrl course
+            ]
+                |> JE.object
+                |> fromType "lia"
+
+
+fromName : { node | name : String } -> ( String, JE.Value )
+fromName node =
+    ( "name", JE.string node.name )
+
+
+fromUrl : { node | url : String } -> ( String, JE.Value )
+fromUrl node =
+    ( "url", JE.string node.url )
+
+
+fromType : String -> JE.Value -> JE.Value
+fromType name obj =
+    JE.object [ ( name, obj ) ]
