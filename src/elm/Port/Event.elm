426c3f2839fd4fe7e3f89633e5b7e8b2ad79ae85
André Dietrich <andredietrich@web.de>
2021-10-27 13:24:28 +0200

    refact(Event): Changed structure to routes & msg

--- src/elm/Port/Event.elm
+++ src/elm/Port/Event.elm
@@ -2,60 +2,66 @@ module Port.Event exposing
     ( Event
     , decode
     , encode
+    , init
     , store
     )
 
 import Json.Decode as JD
 import Json.Encode as JE
+import Lia.Markdown.Chart.Types exposing (Point)
 
 
 type alias Event =
-    { topic : String
-    , section : Int
+    { route : List Point
     , message : JE.Value
     }
 
 
+type Point
+    = Point String (Maybe Int)
+
+
+init : String -> JE.Value -> Event
+init topic =
+    Event [ Point topic Nothing ]
+
+
 encode : Event -> JE.Value
-encode { topic, section, message } =
+encode { route, message } =
     JE.object
-        [ ( "topic", JE.string topic )
-        , ( "section", JE.int section )
+        [ ( "route", JE.list encPoint route )
         , ( "message", message )
         ]
 
 
+encPoint : Point -> JE.Value
+encPoint (Point topic id) =
+    JE.object
+        [ ( "topic", JE.string topic )
+        , ( "id"
+          , id
+                |> Maybe.map JE.int
+                |> Maybe.withDefault JE.null
+          )
+        ]
 
-{-
-   unzip : List JE.Value -> ( List JE.Value, List JE.Value )
-   unzip =
-       List.foldl
-           (\list ( extern, base ) ->
-               case list of
-                   [] ->
-                       ( extern, base )
 
-                   e :: es ->
-                       if isBase e then
-                           ( extern, e :: base )
-
-                       else
-                           ( e :: extern, base )
-           )
-           ( [], [] )
--}
+decPoint : JD.Decoder Point
+decPoint =
+    JD.map2 Point
+        (JD.field "topic" JD.string)
+        (JD.field "id" (JD.maybe JD.int))
 
 
 decode : JD.Value -> Result JD.Error Event
 decode =
     JD.decodeValue
-        (JD.map3 Event
-            (JD.field "topic" JD.string)
-            (JD.field "section" JD.int)
+        (JD.map2 Event
+            (JD.field "route" (JD.list decPoint))
             (JD.field "message" JD.value)
         )
 
 
 store : JE.Value -> Event
 store =
-    Event "store" -1
+    init "store"
