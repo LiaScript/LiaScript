6d290dc769cb8e09d466c9b9d26fc6d19fb3c9a8
André Dietrich <andredietrich@web.de>
2022-01-19 15:15:15 +0100

    doc: Translation module/service

--- src/elm/Service/Translate.elm
+++ src/elm/Service/Translate.elm
@@ -4,6 +4,13 @@ import Json.Encode as JE
 import Service.Event as Event exposing (Event)
 
 
+{-| Simply send this event to the external service `Translate.ts` to enforce a
+translation of the entire site by google translate. This will result in a code
+injection of the google-translate API.
+
+_Other translate services might be implemented in the future._
+
+-}
 google : Event
 google =
     { cmd = "google", param = JE.null }
--- src/typescript/liascript/service/Translate.ts
+++ src/typescript/liascript/service/Translate.ts
@@ -1,7 +1,12 @@
 import log from '../log'
 
+// Checks if there has been a code injection jet
 var googleTranslate = false
 
+/**
+ * This is required to hide the DOM elements that are injected by the
+ * Google-translation API.
+ */
 function changeGoogleStyles() {
   let goog = document.getElementById(':1.container')
 
@@ -11,34 +16,75 @@ function changeGoogleStyles() {
   }
 }
 
+/**
+ * Bootstrap for the google-translation API, defines the main configurations.
+ *
+ * @see <https://www.w3schools.com/howto/howto_google_translate.asp>
+ */
+function googleTranslateElementInit() {
+  // @ts-ignore: will be injected by google
+  new google.translate.TranslateElement(
+    {
+      // use the course lang definition as default
+      pageLanguage: document.documentElement.lang,
+      // includedLanguages: 'ar,en,es,jv,ko,pa,pt,ru,zh-CN',
+      // layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL,
+      // multilanguagePage: true,
+      // gaTrack: true,
+      // gaId: 'todo'
+      autoDisplay: false,
+    },
+    // this defines the `id` of the HTML element that will be replaced by
+    // the google-drop-down field for selection languages
+    'google_translate_element'
+  )
+}
+
+/**
+ * Inject the Google translation API into the head of the document.
+ */
 function injectGoogleTranslate() {
   // inject the google translator
   if (!googleTranslate) {
     let tag = document.createElement('script')
+
+    // TODO:
+    // the general src attribute without protocol needs to be checked with
+    // other protocols IPFS, Hyper, etc.
+    //
     tag.src =
       '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
     tag.type = 'text/javascript'
     document.head.appendChild(tag)
 
-    window.googleTranslateElementInit = function () {
-      // @ts-ignore: will be injected by google
-      new google.translate.TranslateElement(
-        {
-          pageLanguage: document.documentElement.lang,
-          // includedLanguages: 'ar,en,es,jv,ko,pa,pt,ru,zh-CN',
-          // layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL,
-          autoDisplay: false,
-        },
-        'google_translate_element'
-      )
-    }
+    // Setup the global init function, this function is called by google as a
+    // bootstrap and the name has to mach the `cp` parameter of the upper
+    // resource.
+    window.googleTranslateElementInit = googleTranslateElementInit
+
+    // from now on there won't be ne translations
     googleTranslate = true
   }
 }
 
-const Service = {
-  PORT: 'translate',
+const Port = 'translate'
 
+/**
+ * Service handler for **translations**, currently only google is supported.
+ */
+const Service = {
+  /**
+   * Service identifier 'translate', that is used to while service routing.
+   */
+  PORT: Port,
+
+  /**
+   * This adds a special mutation observer for the `lang` attribute within the
+   * `<html>` tag. Whenever there is an external system that translates the
+   * website, then the change of the `lang` attribute is send back to LiaScript
+   * via the `elmSend` function.
+   * @param elmSend - callback function for reaching out LiaScript
+   */
   init: function (elmSend: Lia.Send) {
     var observer = new MutationObserver(function (mutations) {
       mutations.forEach(function (mutation) {
@@ -47,7 +93,7 @@ const Service = {
         elmSend({
           reply: true,
           track: [],
-          service: 'translate',
+          service: Port,
           message: { cmd: 'lang', param: document.documentElement.lang },
         })
       })
@@ -57,12 +103,15 @@ const Service = {
       attributes: true,
       childList: false,
       characterData: false,
+      // only observe changes of this specific attribute
       attributeFilter: ['lang'],
     })
-
-    googleTranslate = false
   },
 
+  /**
+   * Generic handler for event-routing.
+   * @param event
+   */
   handle: function (event: Lia.Event) {
     switch (event.message.cmd) {
       case 'google':
