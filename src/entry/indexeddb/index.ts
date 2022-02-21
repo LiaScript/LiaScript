6c2d8908b6daa6e6eef7a66afc26ac277e236811
André Dietrich <andredietrich@web.de>
2022-02-21 15:00:52 +0100

    Add indexedDB version for IMS export

---
+++ src/entry/indexeddb/index.ts
@@ -0,0 +1,32 @@
+import LiaScript from '../../typescript/liascript/index'
+
+import('../../typescript/connectors/Browser/index').then(function (Browser) {
+  class Connector extends Browser.Connector {
+    hasIndex(): boolean {
+      return false
+    }
+  }
+
+  const ua = window.navigator.userAgent
+
+  if (ua.indexOf('Trident/') > 0 || ua.indexOf('MSIE ') > 0) {
+    console.warn('unsupported browser')
+
+    const elem = document.getElementById('IE-message')
+
+    if (elem) elem.hidden = false
+  } else {
+    let debug = false
+
+    if (process.env.NODE_ENV === 'development') {
+      debug = true
+    }
+
+    const app = new LiaScript(
+      document.body,
+      new Connector(),
+      false, // allowSync
+      debug
+    )
+  }
+})
