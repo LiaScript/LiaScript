b77cec032f223c065bfe71cd60cb329637b68b6e
André Dietrich <andredietrich@web.de>
2022-10-13 21:08:56 +0200

    fix: workbox navigation, exclude cache

--- src/typescript/sw.js
+++ src/typescript/sw.js
@@ -15,6 +15,13 @@ workbox.core.skipWaiting()
 workbox.core.clientsClaim()
 
 // workbox.googleAnalytics.initialize();
+workbox.routing.registerRoute(
+  // Match all navigation requests, except those for URLs whose
+  // path starts with '/admin/'
+  ({request, url}) => request.mode === 'navigate' &&
+                      !url.pathname.startsWith('/LiveEditor/'),
+  new workbox.strategies.StaleWhileRevalidate()
+);
 
 workbox.routing.registerRoute(/\/$/, new workbox.strategies.NetworkFirst())
 workbox.routing.registerRoute(/\/*/, new workbox.strategies.NetworkFirst())
