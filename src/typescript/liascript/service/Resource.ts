1057d26992c1d6cd26f7e92bc85cb6b017288b81
André Dietrich <andredietrich@web.de>
2022-10-25 10:24:24 +0200

    improve: Loading script and style as blob
    If loading relative scripts fails, since github and others will deliver
    them in the wrong mime-type, a second loading is done, which will fetch
    the content and create a Blob url with the appropriate mime-type to be
    loaded.

--- src/typescript/liascript/service/Resource.ts
+++ src/typescript/liascript/service/Resource.ts
@@ -43,6 +43,14 @@ export const Service = {
  * @param withSemaphore - indicates it the global semaphore should be applied.
  */
 export function loadScript(url: string, withSemaphore = false) {
+  const file = new URL(url)
+
+  // try to load all local scripts as blobs
+  if (!url.startsWith('blob:') && file.origin === window.location.origin) {
+    loadScriptAsBlob(url, withSemaphore)
+    return
+  }
+
   try {
     let tag = document.createElement('script')
 
@@ -63,14 +71,89 @@ export function loadScript(url: string, withSemaphore = false) {
       }
       tag.onerror = function (e: any) {
         window.LIA.eventSemaphore--
-        log.warn('could not load =>', url, e)
+        console.warn('something went wrong', e)
       }
     }
 
     document.head.appendChild(tag)
   } catch (e) {
     log.warn('failed loading script => ', e)
+    loadScriptAsBlob(url, withSemaphore)
   }
+}
+
+function loadScriptAsBlob(url: string, withSemaphore) {
+  if (!url.startsWith('blob:')) {
+    loadAsBlob(
+      'script',
+      url,
+      (blobUrl: string) => {
+        loadScript(blobUrl, withSemaphore)
+      },
+      withSemaphore
+        ? (_url, _error) => {
+            window.LIA.eventSemaphore--
+          }
+        : undefined
+    )
+  }
+}
+
+function loadAsBlob(
+  tag: string,
+  url: string,
+  onOk: (blobUrl: string) => void,
+  onError?: (url: string, e: any) => void
+) {
+  if (url.startsWith('blob:')) {
+    console.warn('failed to load blob', url)
+    return
+  }
+
+  let type = 'text/'
+  switch (tag) {
+    case 'script':
+      type += 'javascript'
+      break
+    case 'link':
+      type += 'css'
+      break
+    default:
+      type += 'plain'
+  }
+
+  fetch(url)
+    .then((response) => {
+      const header = response.headers.get('Content-Disposition')
+
+      let match = header?.match('filename="([^"]+)"')
+      let filename = ''
+
+      if (match && match.length > 0) {
+        filename = match[1]
+      }
+
+      const src = new URL(url)
+
+      if (filename !== src.pathname.split('/').slice(-1)[0]) {
+        throw new Error(
+          `false redirect received "${filename}" instead of "${src.pathname}"`
+        )
+      }
+
+      return response.text()
+    })
+    .then((text: string) => {
+      const blob = new Blob([text], { type })
+      const blobUrl = window.URL.createObjectURL(blob)
+      onOk(blobUrl)
+    })
+    .catch((e) => {
+      log.warn('could not load', url, 'as blob =>', e.message)
+      if (onError) {
+        onError(url, e)
+      }
+    })
 }
 
 /**
@@ -83,9 +166,24 @@ function loadLink(url: string) {
     let tag = document.createElement('link')
     tag.href = url
     tag.rel = 'stylesheet'
+    tag.type = 'text/css'
+
+    tag.onerror = (event) => {
+      loadAsBlob('link', url, (blobUrl: string) => {
+        loadLink(blobUrl)
+      })
+    }
 
     document.head.appendChild(tag)
   } catch (e: any) {
-    log.warn('failed loading style => ', e.message)
+    if (url.startsWith('blob:')) {
+      log.warn('failed loading style => ', url, e.message)
+    } else {
+      log.warn('could not load =>', url, e)
+      log.warn('will try to import as blob')
+      loadAsBlob('link', url, (blobUrl: string) => {
+        loadLink(blobUrl)
+      })
+    }
   }
 }
