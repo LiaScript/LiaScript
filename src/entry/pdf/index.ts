4fd0621f794530d2240e3572cbb32dcf86a5d4cf
André Dietrich <andredietrich@web.de>
2022-11-02 14:20:04 +0100

    fix pdf view

--- src/entry/pdf/index.ts
+++ src/entry/pdf/index.ts
@@ -4,7 +4,6 @@ import('../../typescript/connectors/Base/index').then(function (Base) {
   let debug = false
 
   var app = new Lia.LiaScript(
-    document.body,
     new Base.Connector(),
     false, // allowSync
     false, // debug
