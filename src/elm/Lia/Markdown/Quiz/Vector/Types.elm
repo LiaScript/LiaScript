2d5bef7ba6e4e4ee5a9bfe0d3ee2eb1d0266fe09
André Dietrich <andredietrich@web.de>
2023-02-08 18:08:01 +0100

    Quiz: Add clearOnLoad if param  is set

--- src/elm/Lia/Markdown/Quiz/Vector/Types.elm
+++ src/elm/Lia/Markdown/Quiz/Vector/Types.elm
@@ -38,7 +38,7 @@ comp : Quiz -> State -> Bool
 comp quiz state =
     case ( quiz.solution, state ) of
         ( SingleChoice list1, SingleChoice list2 ) ->
-            List.map2 (\l1 l2 -> l1 == l2) list1 list2
+            List.map2 (\l1 l2 -> l1 && l2) list1 list2
                 |> List.any identity
 
         ( MultipleChoice list1, MultipleChoice list2 ) ->
