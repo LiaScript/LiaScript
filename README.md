6be6403f2be093b0407ca0f86d5ecbe91c68622e
André Dietrich <andredietrich@web.de>
2024-09-27 13:14:37 +0200

    Version bump to 0.16.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 author:   André Dietrich
 email:    LiaScript@web.de
-version:  0.15.12
+version:  0.16.0
 language: en
 narrator: UK English Male
 
--- src/typescript/liascript/init.ts
+++ src/typescript/liascript/init.ts
@@ -7,7 +7,7 @@ export function initGlobals() {
   }
 
   if (!window.LIA.version) {
-    window.LIA.version = '0.15.12'
+    window.LIA.version = '0.16.0'
   }
 
   if (!window.LIA.eventSemaphore) {
