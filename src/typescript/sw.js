820cc504ef8606be0976f213d942eacf77e1511a
André Dietrich <andredietrich@web.de>
2025-02-22 15:50:46 +0100

    Revert "updated service worker for kaios"
    This reverts commit 22aee49efa76fe3f9e10fd94f66f773d319081c1.

--- src/typescript/sw.js
+++ src/typescript/sw.js
@@ -1,33 +1,51 @@
-import { clientsClaim, cacheNames } from 'workbox-core'
-import { registerRoute } from 'workbox-routing'
-import { NetworkFirst, CacheFirst } from 'workbox-strategies'
-import { precacheAndRoute } from 'workbox-precaching'
 console.log('service-worker.js')
-// (setConfig removed; in production builds, debug logging is disabled automatically)
-// Updating SW lifecycle to update the app after a user-triggered refresh
+
+// advanced config for injectManifest approach
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js'
+)
+
+// Detailed logging is very useful during development
+workbox.setConfig({
+  debug: false,
+})
+
+// Updating SW lifecycle to update the app after user triggered refresh
 self.skipWaiting()
-clientsClaim()
+workbox.core.clientsClaim()
+
 // data-uris shall not be cached and also work offline
-registerRoute(
+workbox.routing.registerRoute(
   ({ url }) => url.search.startsWith('?data:text'),
+
   async ({ event }) => {
-    const cache = await caches.open(cacheNames.precache)
+    const cache = await caches.open(workbox.core.cacheNames.precache)
     const response = await cache.match(event.request.url.split('?data:text')[0])
+
     return response || fetch(event.request)
   }
 )
+
 // workbox.googleAnalytics.initialize();
-registerRoute(
+workbox.routing.registerRoute(
   // Match all navigation requests, except those for URLs whose
-  // path starts with '/LiveEditor/'
+  // path starts with '/admin/'
   ({ request, url }) =>
     request.mode === 'navigate' && !url.pathname.startsWith('/LiveEditor/'),
-  new NetworkFirst()
+  new workbox.strategies.NetworkFirst()
 )
-registerRoute(/.*/, new NetworkFirst())
-registerRoute(/https:\/\/code\.responsivevoice\.org/, new NetworkFirst())
-registerRoute(
+
+//workbox.routing.registerRoute(/\/*/, new workbox.strategies.NetworkFirst())
+workbox.routing.registerRoute(/.*/, new workbox.strategies.NetworkFirst())
+
+workbox.routing.registerRoute(
+  /https:\/\/code\.responsivevoice\.org/,
+  new workbox.strategies.NetworkFirst()
+)
+
+workbox.routing.registerRoute(
   'https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js',
-  new CacheFirst()
+  new workbox.strategies.CacheFirst()
 )
-precacheAndRoute(self.__WB_MANIFEST)
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
