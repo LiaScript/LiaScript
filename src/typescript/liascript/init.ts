44b686ba2c800753d58f7fc5cfc02c3a3cb1e1d7
André Dietrich <andredietrich@web.de>
2025-03-06 21:27:32 +0100

    Version bump to 0.16.11

--- src/typescript/liascript/init.ts
+++ src/typescript/liascript/init.ts
@@ -7,7 +7,7 @@ export function initGlobals() {
   }
 
   if (!window.LIA.version) {
-    window.LIA.version = '0.16.10'
+    window.LIA.version = '0.16.11'
   }
 
   if (!window.LIA.eventSemaphore) {
