20b94c37e00daaec38c680a8af1b4c4b5b79ed0d
André Dietrich <andredietrich@web.de>
2025-08-15 08:14:00 +0200

    fix: pdf export

--- src/entry/pdf/index.ts
+++ src/entry/pdf/index.ts
@@ -3,10 +3,9 @@ import * as Lia from '../../typescript/liascript/index'
 import('../../typescript/connectors/Base/index').then(function (Base) {
   let debug = false
 
-  var app = new Lia.LiaScript(
-    new Base.Connector(),
-    false, // allowSync
-    false, // debug
-    true // fullPage
-  )
+  var app = new Lia.LiaScript(new Base.Connector(), {
+    allowSync: false,
+    debug: false,
+    fullPage: true,
+  })
 })
